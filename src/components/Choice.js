08482a6ea75976e1899050ee3ffcc9759881fa12
tenessy0570 <tenessy0570@gmail.com>
2024-12-01 18:20:53 +0700

    refactored choice component

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -4,39 +4,65 @@ import PropTypes from "prop-types";
 class Choice extends Component {
     constructor(props) {
         super(props);
-        this.state = { choice: props.choice };
+        this.state = {choice: props.choice};
 
         this.handleChoiceChange = this.handleChoiceChange.bind(this);
     }
+
     handleChoiceChange(event) {
         if (event.target.classList.contains("direction_variant")) {
-            this.setState({choice: "class"});
+            this.setState(
+                {
+                    choice: "class",
+                    chosenDirection: event.target.textContent
+                }
+            );
+            return null;
+        }
+        if (event.target.classList.contains("class_variant")) {
+            this.setState(
+                {
+                    choice: "spec",
+                    chosenDirection: this.state.chosenDirection,
+                    chosenClass: event.target.textContent
+                }
+            )
+            return null;
+        }
+        if (event.target.classList.contains("spec_variant")) {
+            this.setState(
+                {
+                    choice: null,
+                    chosenDirection: this.state.chosenDirection,
+                    chosenClass: this.state.chosenClass,
+                    chosenSpec: event.target.textContent
+                }
+            );
+            return null;
         }
     }
+
     renderDirectionChoice() {
+        //let availableDirections = ...RETRIEVE FROM API;
         let availableDirections = ["PVE", "PVP"];
-        return (
-            <div>
-                <p className="text-dark text-center fs-2">Choose a direction.</p>
-                <div className="row">
-                    <div className="col-sm-3"></div>
-                    <div className="btn-group-vertical text-center col-sm-6">
-                        {
-                            availableDirections.map(
-                                (direction, index) => (
-                                    <button onClick={this.handleChoiceChange} key={index} className="direction_variant btn btn-outline-primary">{direction}</button>
-                                )
-                            )
-                        }
-                    </div>
-                    <div className="col-sm-3"></div>
-                </div>
-            </div>
+        return this.renderAbstractChoice("direction_variant", availableDirections, this.handleChoiceChange);
 
-        )
     }
+
     renderClassChoice() {
+        // let availableGameClasses = ...RETRIEVE FROM API;
         let availableGameClasses = ["Warrior", "Druid"];
+        return this.renderAbstractChoice("class_variant", availableGameClasses, this.handleChoiceChange);
+
+    }
+
+    renderSpecChoice() {
+        // let availableClassSpecs = ...RETRIEVE FROM API;
+        let availableClassSpecs = ["Frost", "Fire", "Arcane"];
+        return this.renderAbstractChoice("spec_variant", availableClassSpecs, this.handleChoiceChange);
+    }
+
+    renderAbstractChoice(additionalButtonClass, elementsList, clickHandler) {
         return (
             <div>
                 <p className="text-dark text-center fs-2">Choose a direction.</p>
@@ -44,9 +70,10 @@ class Choice extends Component {
                     <div className="col-sm-3"></div>
                     <div className="btn-group-vertical text-center col-sm-6">
                         {
-                            availableGameClasses.map(
-                                (gameClass, index) => (
-                                    <button key={index} className="class_variant btn btn-outline-primary">{gameClass}</button>
+                            elementsList.map(
+                                (item, index) => (
+                                    <button onClick={clickHandler} key={index}
+                                            className={`${additionalButtonClass} btn btn-outline-primary`}>{item}</button>
                                 )
                             )
                         }
@@ -57,22 +84,28 @@ class Choice extends Component {
 
         )
     }
+
     render() {
         switch (this.state.choice) {
             case "direction":
                 return this.renderDirectionChoice();
             case "class":
                 return this.renderClassChoice();
+            case "spec":
+                return this.renderSpecChoice();
             default:
                 return (
                     <div>
-                        <p>No choice selected.</p>
+                        <p className="text-dark">
+                            Chosen guides
+                            filter: {this.state.chosenSpec} {this.state.chosenClass} {this.state.chosenDirection}.
+                        </p>
                     </div>
                 );
         }
     }
 }
 
-Choice.defaultProps = { choice: "direction" };
-Choice.propTypes = { choice: PropTypes.string };
+Choice.defaultProps = {choice: "direction"};
+Choice.propTypes = {choice: PropTypes.string};
 export default Choice;
